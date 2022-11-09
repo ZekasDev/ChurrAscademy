f78b9ca5ab44a46eee63341f179dde020b4fe73f
ZekasDev <dannndourado@gmail.com>
2022-11-09 17:58:22 -0300

    Card player DONE

--- src/Pages/Participants/Participants.jsx
+++ src/Pages/Participants/Participants.jsx
@@ -7,7 +7,16 @@ export function Participants() {
     <div className="participants">
       <Header />
       <div className="participants_bg">
-        <div className="players_container"></div>
+        <div className="players_container">
+          <div className="card_player">
+            <div className="player">
+              <img src="../../src/assets/imgs/test.jpg" alt="" />            </div>
+            <div className="player_infos">
+              <h3>Calvin Barreto</h3>
+              <p>Aqui é o Beterraba, Mestre do PMA. Só quando estou na party, Pablo tem chances de ganhar.</p> 
+            </div>         
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
