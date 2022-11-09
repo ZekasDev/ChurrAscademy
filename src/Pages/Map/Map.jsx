2824c707daa4da759c7d557dc3fb11718206bc95
ZekasDev <dannndourado@gmail.com>
2022-11-09 18:12:51 -0300

    75% winrate

--- src/Pages/Map/Map.jsx
+++ src/Pages/Map/Map.jsx
@@ -1,10 +1,13 @@
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import "./Map.scss";
 
 export function Map() {
   return (
     <div className="map">
       <Header />
+      MAPA
+      <Footer />
     </div>
   );
 }
