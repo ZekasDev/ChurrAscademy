58cb659826e10dcd7c944c7c7e20ca59215c20d6
LucasLaricchia <lucaslaricchia@gmail.com>
2022-11-11 02:17:46 -0300

    site completamente responsivo

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -8,7 +8,10 @@ export function Map() {
       <Header />
       <div className="map_bg">
         <div className="map_container">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d248.0104128633579!2d-35.1271185067702!3d-5.971780170953974!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1668074412978!5m2!1spt-BR!2sbr"></iframe>
+          <iframe
+            className="map_iframe"
+            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d248.0104128633579!2d-35.1271185067702!3d-5.971780170953974!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1668074412978!5m2!1spt-BR!2sbr"
+          ></iframe>
           <div className="map_description">
             <span>
               Seguindo o sentido "Cajueiro/Natal", você vai descer a ladeira que
